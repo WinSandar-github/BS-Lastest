3fb5480dc7eeefce9e1e992777f0a32ed2c2a0a5
DESKTOP-BKLVCMT\Twetar <twetar>
2021-10-05 16:07:15 +0630

    modified backend_url.

--- Frontend/Utilities/js/common.js
+++ Frontend/Utilities/js/common.js
@@ -1,12 +1,14 @@
 // BACKEND_URL="https://demo.aggademo.me/bs/Backend/public/";
 //BACKEND_URL="http://localhost/bs/Backend/public/";
-// let INVOICE_URL="https://iqnet.tech/billing/Frontend/Components/Customer/";
-// let BACKEND_URL="https://iqnet.tech/billing/Backend/public/";
+
+let INVOICE_URL="https://iqnet.tech/billing/Frontend/Components/Customer/";
+let BACKEND_URL="https://iqnet.tech/billing/Backend/public/";
+
 // let INVOICE_URL="http://localhost//iqnet/BS/Frontend/Components/Customer/";
 // let BACKEND_URL="http://localhost:8000/";
 
-let INVOICE_URL="http://localhost:1234/BS/Frontend/Components/Customer/"
-let BACKEND_URL = "http://localhost:1234/BS/Backend/public/"
+// let INVOICE_URL="http://localhost:1234/BS/Frontend/Components/Customer/"
+// let BACKEND_URL = "http://localhost:1234/BS/Backend/public/"
 
 var toastOptions = {
     "closeButton": true,
